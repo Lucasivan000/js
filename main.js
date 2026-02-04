20a08fde928043b47108b21ffe83430642440acd
Lucas Cardozo <lucasivancardozo000@gmail.com>
2026-02-04 17:20:05 -0300

    prueba de if

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 const tipoDeSeguro = document.getElementById("tipoDeSeguro");
+const divisiones = document.querySelectorAll(".opciones")
+
+let boton = document.getElementById("boton");
+
+boton.addEventListener("click", function() {
+    const tipoSeleccionado = tipoDeSeguro.value;
+    divisiones.forEach(division => {
+    division.style.display = "none";
+    });    
+    
+    if (tipoSeleccionado){
+         document.getElementById(tipoSeleccionado).style.display = "block";
+
+
+    };
+        
+
+});
+    
+
+
+
 
 
