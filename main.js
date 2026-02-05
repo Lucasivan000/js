d50428d334adb4d92a67dcbf5d4f8284b3e4f85c
Lucas Cardozo <lucasivancardozo000@gmail.com>
2026-02-04 23:48:34 -0300

    variable resultadoA despues de mil años

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+console.log(document);
+console.dir(document);
+
 const tipoDeSeguro = document.getElementById("tipoDeSeguro");
-const divisiones = document.querySelectorAll(".opciones")
+const divisiones = document.querySelectorAll(".opciones");
 
 let boton = document.getElementById("boton");
 
@@ -19,6 +22,22 @@ boton.addEventListener("click", function() {
 });
     
 // calculos // 
+//automotor //
+let resultadoA = document.getElementById("cotizarA");
+
+resultadoA.addEventListener("click", function() {
+    const valorA = document.getElementById("valorAutomotor");
+    const anioA = document.getElementById("anioAutomotor");
+    let cuota;
+      if (anioA.value <= 2026 && anioA.value >= 2016) {
+        cuota = 0.02;
+    } else if (anioA.value < 2016 && anioA.value >= 2006) {
+        cuota = 0.03;
+    } else {
+        alert("No asegurable por el año del vehículo");
+}});
+
+
 
 
 
